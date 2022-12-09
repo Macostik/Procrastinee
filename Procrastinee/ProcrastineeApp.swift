24073de3d2481b895288d604c033e77cce35f1f3
Macostik <yura-granchenko@yandex.ru>
2022-12-09 09:35:04 +0200

    Initial Commit

---
+++ Procrastinee/ProcrastineeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcrastineeApp.swift
+//  Procrastinee
+//
+//  Created by Macostik on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProcrastineeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
